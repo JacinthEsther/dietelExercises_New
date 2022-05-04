a99a1619dd8f733c940a898473c64dabb7106157
Agboniro Esther Jacinta <agbonirojacinta@gmail.com>
2022-05-04 22:32:32 +0100

    updated code

---
+++ src/chapter4/Factorial.java
@@ -0,0 +1,41 @@
+package chapter4;
+
+public class Factorial {
+    int total= 1;
+    double total2=0;
+    int numerator=1;
+    double result;
+    public void calculateFactorialOf(int number) {
+        if(number >0) {
+            for (int i = 1; i <= number; i++) {
+                total *= i;
+            }
+        }
+        else throw new RuntimeException("number must not be negative");
+    }
+
+    public int getFactorial() {
+
+        return total;
+    }
+
+    public void calculateExponentialOf(int number) {
+        if(number >0) {
+            for (int i = 1; i <= number; i++) {
+                for (int j = 1; j < i; j++) {
+                    total*=j;
+                }
+                result = numerator*1.0/total;
+                total2 += result;
+            }
+            total2=1+total2;
+        }
+        else throw new RuntimeException("number must not be negative");
+
+    }
+
+    public double getExponential() {
+        return (double)Math.round(total2*100.0)/100.0;
+
+    }
+}
---
+++ test/chapter4/FactorialTest.java
@@ -0,0 +1,34 @@
+package chapter4;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class FactorialTest {
+
+    Factorial factorial;
+    @BeforeEach
+    void setUp(){
+        factorial = new Factorial();
+    }
+    @Test
+    void testForFactorialOfNonNegativeInteger(){
+
+        factorial.calculateFactorialOf(5);
+
+        assertEquals(120, factorial.getFactorial());
+    }
+    @Test
+    void negativeIntegerThrowsException(){
+        assertThrows(RuntimeException.class,()-> factorial.calculateFactorialOf(-1));
+    }
+
+    @Test
+    void testForTheExponentialOfANonNegativeInteger(){
+        factorial.calculateExponentialOf(5);
+        assertEquals(3.59, factorial.getExponential());
+
+    }
+}
